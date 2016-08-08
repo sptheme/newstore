13ca74dfcfc9123953effc005de8762975da7a2e
sptheme <sopheak.peas@gmail.com>
2016-08-08 16:36:40 +0700

    Adding starter theme with Redux & Meta box framework

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package newstore
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'newstore' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'newstore' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'newstore' ), 'newstore', '<a href="https://www.linkedin.com/in/sopheakpeas" rel="designer">Sopheak Peas</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
