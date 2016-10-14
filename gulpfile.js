d4922829383ebc33e85c3e313d197bd1a2687868
sptheme <sopheak.peas@gmail.com>
2016-10-14 16:26:09 +0700

    Using import woocommerce.scss in theme.scss

--- gulpfile.js
+++ gulpfile.js
@@ -94,9 +94,6 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
     gulp.watch('./sass/**/*.scss', ['sass']);
     gulp.watch('./css/theme.css', ['cssnano']);
-
-    // Support wooCommerce
-    gulp.watch('./css/woocommerce.css', ['cssnano']);
 });
 
 // Run: 
@@ -116,7 +113,6 @@ gulp.task('cssnano', ['cleancss'], function(){
 gulp.task('cleancss', function() {
   return gulp.src('./css/*.min.css', { read: false }) // much faster 
     .pipe(ignore('theme.css'))
-    .pipe(ignore('woocommerce.css'))
     .pipe(rimraf());
 });
 
