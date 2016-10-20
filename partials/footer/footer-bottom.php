ae75989598f8be04d9fbbaba4100c3044523a3cd
sptheme <sopheak.peas@gmail.com>
2016-10-20 16:30:42 +0700

    B4 markup for Footer Navigation

--- partials/footer/footer-bottom.php
+++ partials/footer/footer-bottom.php
@@ -16,19 +16,9 @@ $copyright = wpsp_get_redux( 'footer-copyright-text', 'Copyright <a href="#">You
 // WPML translations
 $copyright = wpsp_translate_theme_mod( 'footer-copyright-text', $copyright ); ?>
 
-<div id="footer-bottom" class="clearfix"<?php wpsp_schema_markup( 'footer_bottom' ); ?>>
+<div class="footer-bottom"<?php wpsp_schema_markup( 'footer_bottom' ); ?>>
 
-	<div id="footer-bottom-inner" class="container">
-
-		<?php
-		// Display copyright info
-		if ( $copyright ) : ?>
-
-			<div id="copyright" class="clearfix" role="contentinfo">
-				<?php echo do_shortcode( $copyright ); ?>
-			</div><!-- #copyright -->
-
-		<?php endif; ?>
+	<div class="footer-bottom-inner container">
 
 		<?php
 		// Get footer menu location and apply filters for child theming
@@ -37,20 +27,30 @@ $copyright = wpsp_translate_theme_mod( 'footer-copyright-text', $copyright ); ?>
 
 		// Display footer bottom menu if location is defined
 		if ( has_nav_menu( $menu_location ) ) : ?>
+		<div id="nav-footer" class="text-xs-center">
+			<?php
+			// Display footer menu
+			wp_nav_menu( array(
+				'menu_class' 	 => 'nav nav-inline',
+				'container'		 => 'nav',
+				'container_id'   => 'nav-footer',
+				'fallback_cb'    => '',
+				'theme_location' => $menu_location,
+				'walker' => new wp_bootstrap_navwalker()
+			) ); ?>
+		</div> <!-- #nav-footer -->
+		<?php endif; ?>
 
-			<div id="footer-bottom-menu" class="clearfix">
-				<?php
-				// Display footer menu
-				wp_nav_menu( array(
-					'theme_location' => $menu_location,
-					'sort_column'    => 'menu_order',
-					'fallback_cb'    => false,
-				) ); ?>
+		<?php
+		// Display copyright info
+		if ( $copyright ) : ?>
 
-			</div><!-- #footer-bottom-menu -->
+			<div id="copyright" role="contentinfo" class="text-xs-center">
+				<?php echo do_shortcode( $copyright ); ?>
+			</div><!-- #copyright -->
 
 		<?php endif; ?>
 
-	</div><!-- #footer-bottom-inner -->
+	</div><!-- .footer-bottom-inner -->
 
-</div><!-- #footer-bottom -->
+</div><!-- .footer-bottom -->
