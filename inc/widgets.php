324df94ab7e10439557ff3a8f7bda7275a193db8
sptheme <sopheak.peas@gmail.com>
2016-10-20 17:27:18 +0700

    Add mailchimp widget newsletter

--- inc/widgets.php
+++ inc/widgets.php
@@ -148,42 +148,34 @@ function wpsp_widgets_init() {
 add_action( 'widgets_init', 'wpsp_widgets_init' );
 
 /**
- * Setup default sidebar of each pages and posts
+ * Include all custom widget classes
  *
- * @version 1.0.0
+ * @since 12.0.0
  */
-if ( ! function_exists( 'wpsp_sidebar_primary' ) ) :    
-function wpsp_sidebar_primary() {
-    
-    $sidebar = 'sidebar-1';
+function custom_widgets() {
 
-    // Set sidebar based on page
-    if ( is_home() && wpsp_get_redux('sidebar-global') ) $sidebar = wpsp_get_redux('sidebar-global');
+    // Define directory for widgets
+    $dir = get_template_directory() .'/inc/widgets/';
 
-    if ( is_single() && wpsp_get_redux('sidebar-single') ) $sidebar = wpsp_get_redux('sidebar-single');
-    if ( is_archive() && wpsp_get_redux('sidebar-archive') ) $sidebar = wpsp_get_redux('sidebar-archive');
-    if ( is_category() && wpsp_get_redux('sidebar-category') ) $sidebar = wpsp_get_redux('sidebar-category');
-    if ( is_search() && wpsp_get_redux('sidebar-search') ) $sidebar = wpsp_get_redux('sidebar-search');
-    if ( is_404() && wpsp_get_redux('sidebar-404') ) $sidebar = wpsp_get_redux('sidebar-404');
-    if ( is_page() && wpsp_get_redux('sidebar-page') ) $sidebar = wpsp_get_redux('sidebar-page');
+    // Define array of custom widgets for the theme so you can easily disable any
+    $widgets = array(
+        'newsletter',
+    );
 
-    /***
-     * FILTER    => Add filter for tweaking the sidebar display via child theme's
-     * IMPORTANT => Must be added before meta options so that it doesn't take priority
-     ***/
-    $sidebar = apply_filters( 'wpsp_sidebar_primary', $sidebar );
-
-    // Check for page/post specific sidebar
-    if ( is_page() || is_single() ) {
-        // Reset post data
-        wp_reset_postdata();
-        global $post;
-        // Get meta
-        $meta = get_post_meta($post->ID,'wpsp_sidebar_primary',true);
-        if ( $meta ) { $sidebar = $meta; }
+    // Add templatera widget
+    if ( function_exists( 'templatera_init' ) ) {
+        $widgets['templatera'] = 'templatera';
     }
 
-    // Return sidebar
-    return $sidebar;
+    // Apply filters
+    $widgets = apply_filters( 'wpsp_custom_widgets', $widgets );
+
+    // Loop through array and register the custom widgets
+    if ( $widgets && is_array( $widgets ) ) {
+        foreach ( $widgets as $widget ) {
+            require_once( $dir . $widget .'.php' );
+        }
+    }
+
 }
-endif;
+add_action( 'widgets_init', 'custom_widgets' );
